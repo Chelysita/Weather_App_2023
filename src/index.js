eb7f584db87272c81e504a0b557d678fe3ee4aff
Chelysita <ara.medinago@gmail.com>
2023-03-06 14:20:49 -0500

    Images added and unit temp adjusted

--- src/index.js
+++ src/index.js
@@ -1,7 +1,3 @@
-let apikey = "cdc6f40eaa51d2e0ae19d310a7a3769c";
-let city = "Paris";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
-
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
@@ -27,7 +23,8 @@ function formatDate(timestamp) {
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
-  temperatureElement.innerHTML = Math.round(response.data.main.temp);
+  celsiusTemp = response.data.main.temp;
+  temperatureElement.innerHTML = Math.round(celsiusTemp);
   let cityElement = document.querySelector("#cityInput");
   cityElement.innerHTML = response.data.name;
   let descriptionElement = document.querySelector("#weatherDescription");
@@ -38,31 +35,53 @@ function displayTemperature(response) {
   windElement.innerHTML = Math.round(response.data.wind.speed);
   let dateElement = document.querySelector("#displayDate");
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  let iconElement = document.querySelector("#icon");
+  iconElement.setAttribute(
+    "src",
+    `images/${response.data.weather[0].icon}.png`
+  );
 }
-axios.get(apiUrl).then(displayTemperature);
 
-function search(event) {
+function search(city) {
+  let apikey = "cdc6f40eaa51d2e0ae19d310a7a3769c";
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apikey}&units=metric`;
+  let apiHourUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apikey}&units=metric`;
+  console.log(apiHourUrl);
+  axios.get(apiUrl).then(displayTemperature);
+}
+function handleSubmit(event) {
   event.preventDefault();
   let newcity = document.querySelector("#city-NewInput");
   let cityElement = document.querySelector("#cityInput");
-  cityElement.innerHTML = `${newcity.value}`;
+  search(newcity.value);
 }
+
 let changeCity = document.querySelector("#form");
-changeCity.addEventListener("submit", search);
+changeCity.addEventListener("submit", handleSubmit);
+
+let celsiusTemp = null;
 
 function changeToFarenheit(event) {
   event.preventDefault();
+  let farenheitTemp = Math.round(celsiusTemp) * (9 / 5) + 32;
+  celsiusLink.classList.remove("unitColor");
+  farenheitLink.classList.add("unitColor");
   let toFarenheit = document.querySelector("#temperature");
-  toFarenheit.innerHTML = "30";
+  toFarenheit.innerHTML = Math.round(farenheitTemp);
 }
 
 function changeToCelsius(event) {
   event.preventDefault();
   let toCelsius = document.querySelector("#temperature");
-  toCelsius.innerHTML = "12";
+  celsiusLink.classList.add("unitColor");
+  farenheitLink.classList.remove("unitColor");
+  toCelsius.innerHTML = Math.round(celsiusTemp);
 }
 let farenheitLink = document.querySelector("#farenheitLink");
 farenheitLink.addEventListener("click", changeToFarenheit);
 
 let celsiusLink = document.querySelector("#celsiusLink");
 celsiusLink.addEventListener("click", changeToCelsius);
+
+search("Boston");
